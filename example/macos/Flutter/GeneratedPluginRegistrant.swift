2fd98e9140ba5f7d60d2f3f5b90c602f65c94367
vanlam <lamvan@hopee.com.vn>
2022-01-21 13:26:31 +0700

    [DEV] Init project

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import sii_printer_plugin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SiiPrinterPlugin.register(with: registry.registrar(forPlugin: "SiiPrinterPlugin"))
+}
---
+++ ios/Classes/SiiPrinterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SiiPrinterPlugin : NSObject<FlutterPlugin>
+@end
